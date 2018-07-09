cccf3602911ec917037e4ec62c1b781e29022731
Jason Swett <jason@benfranklinlabs.com>
2018-07-09 10:24:52 -0400

    Add error message.

--- script.js
+++ script.js
@@ -9,19 +9,24 @@ var toppings = [
 var $submitButton = document.getElementById('submit');
 var $toppingName = document.getElementById('topping-name');
 
+function addToppingRow($content, toppingName) {
+  let $li = document.createElement('li');
+  $li.innerHTML = toppingName;
+  $content.appendChild($li);
+}
+
 function refreshToppingList() {
   let $content = document.getElementById('content');
   $content.innerHTML = '';
 
-  toppings.forEach(function(row) {
-    let $li = document.createElement('li');
-    $li.innerHTML = row;
-    $content.appendChild($li);
+  toppings.forEach(function(toppingName) {
+    addToppingRow($content, toppingName)
   });
 }
 
 function addTopping(toppingName) {
   if (toppingName === '') {
+    setErrorMessage("Topping name can't be blank");
     return;
   }
 
@@ -30,11 +35,17 @@ function addTopping(toppingName) {
   $toppingName.value = '';
 }
 
+function setErrorMessage(errorMessage) {
+  document.getElementById('error').innerHTML = errorMessage;
+}
+
 $submitButton.onclick = function() {
   addTopping($toppingName.value);
 };
 
 $toppingName.onkeypress = function(e) {
+  setErrorMessage('');
+
   if (e.key === 'Enter') {
     addTopping($toppingName.value);
   }
