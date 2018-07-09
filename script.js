0a432b2e8761803e36d696dfc589d14d35448f75
Jason Swett <jason@benfranklinlabs.com>
2018-07-09 08:37:34 -0400

    Get enter press working.

--- script.js
+++ script.js
@@ -7,22 +7,32 @@ var toppings = [
 ];
 
 function refreshToppingList() {
-  let content = document.getElementById('content');
-  content.innerHTML = '';
+  let $content = document.getElementById('content');
+  $content.innerHTML = '';
 
   toppings.forEach(function(row) {
-    let li = document.createElement('li');
-    li.innerHTML = row;
-    content.appendChild(li);
+    let $li = document.createElement('li');
+    $li.innerHTML = row;
+    $content.appendChild($li);
   });
 }
 
-var submitButton = document.getElementById('submit');
-
-submitButton.onclick = function() {
-  let toppingName = document.getElementById('topping-name');
-  toppings.push(toppingName.value);
+function addTopping(toppingName) {
+  toppings.push(toppingName);
   refreshToppingList();
+}
+
+var $submitButton = document.getElementById('submit');
+var $toppingName = document.getElementById('topping-name');
+
+$submitButton.onclick = function() {
+  addTopping($toppingName.value);
+};
+
+$toppingName.onkeypress = function(e) {
+  if (e.key === 'Enter') {
+    addTopping($toppingName.value);
+  }
 };
 
 refreshToppingList();
