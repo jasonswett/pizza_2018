557a0898982218c816e0b8c74fda44a74d9dd057
Jason Swett <jason@benfranklinlabs.com>
2018-07-09 08:17:06 -0400

    Get Add Topping button working.

--- script.js
+++ script.js
@@ -6,16 +6,23 @@ var toppings = [
   'Green Pepper'
 ];
 
-content = document.getElementById('content');
+function refreshToppingList() {
+  let content = document.getElementById('content');
+  content.innerHTML = '';
 
-toppings.forEach(function(row) {
-  let li = document.createElement('li');
-  li.innerHTML = row;
-  content.appendChild(li);
-});
+  toppings.forEach(function(row) {
+    let li = document.createElement('li');
+    li.innerHTML = row;
+    content.appendChild(li);
+  });
+}
 
 submitButton = document.getElementById('submit');
 
 submitButton.onclick = function() {
-  console.log('submit clicked');
+  let toppingName = document.getElementById('topping-name');
+  toppings.push(toppingName.value);
+  refreshToppingList();
 };
+
+refreshToppingList();
